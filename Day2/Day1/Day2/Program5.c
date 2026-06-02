327c7a0a2fab2cd24c00e9e7e9e3d8b4f5bd9a33
Tanishka Jain <25114CN389@glbitm.ac.in>
2026-06-02 21:44:39 +0530

    Fix Day2 folder structure

---
+++ Day2/Day1/Day2/Program5.c
@@ -0,0 +1,13 @@
+//write a program to find the sum of digits of a number
+#include <stdio.h>
+int main(){
+    int num,sum=0;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    while(num>0){
+        sum+=num%10;
+        num/=10;
+    }   
+    printf("The sum of digits is: %d",sum);
+    return 0;
+}
