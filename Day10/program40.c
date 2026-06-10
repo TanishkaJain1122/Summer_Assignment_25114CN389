cd86b4fee8a789a3f846eb5f9a0371281b4cd476
Tanishka Jain <25114CN389@glbitm.ac.in>
2026-06-10 17:45:14 +0530

    Add Day10 Files

---
+++ Day10/program40.c
@@ -0,0 +1,21 @@
+//Alphabet Pyramid
+#include <stdio.h>
+int main() {
+    int n, i, j,space;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for(space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <=i ; j++) {
+            printf("%c ", (char)(64+j));
+
+        }
+        for (j = i-1; j >= 1; j--) {
+            printf("%c ", (char)(64+j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
