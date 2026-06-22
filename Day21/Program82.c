21d7d05770bed5b85fa213015b894ad192208ca9
Tanishka Jain <25114CN389@glbitm.ac.in>
2026-06-22 13:50:25 +0530

    Added Day22 Programs

---
+++ Day21/Program82.c
@@ -0,0 +1,26 @@
+//WAP to reverse a string
+#include <stdio.h>
+
+int main() {
+    char str[100], temp;
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    // Find length of string
+    while(str[length] != '\0') {
+        length++;
+    }
+
+    // Reverse the string
+    for(i = 0; i < length / 2; i++) {
+        temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    printf("Reversed string: %s", str);
+
+    return 0;
+}
---
+++ Day22/Program86.c
@@ -0,0 +1,21 @@
+//WAP to Count words in a Sentence
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, words = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if((i == 0 && str[i] != ' ') ||
+           (str[i] != ' ' && str[i-1] == ' ')) {
+            words++;
+        }
+    }
+
+    printf("Number of words = %d", words);
+
+    return 0;
+}
